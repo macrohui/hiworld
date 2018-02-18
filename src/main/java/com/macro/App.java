ba69b194aada78de2fa35bfccb7e3898f2078dca
macrohui <macrohui29@sina.com>
2018-02-18 11:14:28 +0800

    test

--- src/main/java/com/macro/App.java
+++ src/main/java/com/macro/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World111!" );
+        System.out.println( "111Hello Wo11rld111!" );
     }
 }
